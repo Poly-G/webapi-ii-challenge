517a595ca2ec977dad0acd8e2941aebe2e3033a8
Poly Goveia <poly.goveia@gmail.com>
2019-08-01 16:25:13 -0700

    add post

--- posts/post-router.js
+++ posts/post-router.js
@@ -3,4 +3,26 @@ const db = require("../data/db.js");
 
 const router = express.Router();
 
+// send a POST request to /api/posts to create a new post
+router.post("/", (req, res) => {
+  const { title, contents } = req.body;
+
+  if (!title || !contents) {
+    res
+      .status(400)
+      .json({ errorMessage: "Post must contain a title and contents" });
+  } else {
+    db.insert({ title, contents })
+      .then(() => {
+        res.status(201).json({ title, contents });
+      })
+      .catch(() => {
+        res.status(500).json({
+          errorMessage:
+            "There was an error while saving the post to the database"
+        });
+      });
+  }
+});
+
 module.exports = router;
