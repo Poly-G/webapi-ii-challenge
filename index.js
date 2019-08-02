7236bbf02e014434f789a934a3eb5c22fe2baf4b
Poly Goveia <poly.goveia@gmail.com>
2019-08-01 19:56:10 -0700

    setup for dev

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const server = require("./server.js");
+require("dotenv").config();
 
-const port = process.env.PORT ? process.env.port : 4000;
+const port = process.env.PORT ? process.env.PORT : 4000;
 
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
