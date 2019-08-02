d6a0561ca4ec4f9e136575e4ba17caefa133add5
Poly Goveia <poly.goveia@gmail.com>
2019-08-01 17:23:19 -0700

    add delete

--- posts/post-router.js
+++ posts/post-router.js
@@ -91,4 +91,23 @@ router.get("/:id/comments", async (req, res) => {
   }
 });
 
+// send a DELETE request to /:id to delete a specific post
+router.delete("/:id", async (req, res) => {
+  try {
+    const post = await db.remove(req.params.id);
+    if (post > 0) {
+      res.status(200).json(post);
+    } else {
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist." });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      error: "The post could not be removed"
+    });
+  }
+});
+
 module.exports = router;
